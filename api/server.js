e263a8f614997f012fab5b671c520c14e7a5cca4
Gaston Irby III <gaston@Gastons-iMac.hsd1.wa.comcast.net>
2022-02-21 17:04:48 -0800

    cookie

--- api/server.js
+++ api/server.js
@@ -3,7 +3,9 @@ const helmet = require("helmet");
 const cors = require("cors");
 const usersRouter = require('./users/users-router')
 const authRouter = require('./auth/auth-router.js')
-
+const session = require('express-session')
+const Store = require('connect-session-knex')(session)
+const knex = require('../data/db-config')
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -14,11 +16,30 @@ const authRouter = require('./auth/auth-router.js')
   and a cookie set on the client. The name of the cookie should be "chocolatechip".
 
   The session can be persisted in memory (would not be adecuate for production)
-  or you can use a session store like `connect-session-knex`.
+  or you can use a session Store like `connect-session-knex`.
  */
 
 const server = express();
 
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'shh',
+  saveUninitialized: false,
+  resave: false,
+  store: new Store({
+    knex,
+    createTable: true,
+    clearInterval: 1000 * 60 * 10,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true,
+    // sameSite: 'none'
+  }
+}))
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
