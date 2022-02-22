c19aa2f106f895d9714607fd133fa51df518da2d
Gaston Irby III <gaston@Gastons-iMac.hsd1.wa.comcast.net>
2022-02-21 17:32:43 -0800

    done!

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -9,9 +9,11 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  console.log('restricted')
-  next()
-
+  if (req.session.user) {
+    next()
+  } else {
+    next({ status: 401, message: 'You shall not pass!' })
+  }
 }
 
 /*
@@ -43,7 +45,10 @@ async function checkUsernameFree(req, res, next) {
 async function checkUsernameExists(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username })
-    if (users.length) next()
+    if (users.length) {
+      req.users = users[0]
+    next()
+    }
     else next({ message: "Invalid credentials", status: 401 })
   } catch (err) {
     next(err)
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,8 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 const router = require('express').Router();
+const User = require('../users/users-model')
+const bcrypt = require('bcryptjs')
 const { 
   checkPasswordLength,
   checkUsernameExists,
@@ -32,9 +34,15 @@ const {
 
 
 router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next) => {
-  res.json('register')
+  const { username, password } = req.body
+  const hash = bcrypt.hashSync(password, 8)
+  User.add({ username, password: hash })
+    .then(saved => {
+      res.status(201).json(saved)
+      res.json({ message: 'Password must be longer than 3 chars'})
+    })
+    .catch(next)
 })
-
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -53,7 +61,13 @@ router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next
  */
 
 router.post('/login', checkUsernameExists, (req, res, next) => {
-  res.json('login')
+  const { password } = req.body
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.sess.user = req.user
+    res.json({ message: `Welcome ${req.user.username}`})
+  } else {
+    next({ status: 401, message: 'Invalid credentials'})
+  }
 })
 
 /**
@@ -73,7 +87,18 @@ router.post('/login', checkUsernameExists, (req, res, next) => {
  */
 
 router.get('/logout', (req, res, next) => {
-  res.json('logout')
+  if (req.session.user) {
+    req.session.destroy(err => {
+      if (err) {
+        next(err)
+      } else {
+        res.json({ message: "logged out" })
+      }
+    })
+  } else {
+    res.json({ message: 'no session' })
+  }
+  
 })
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
